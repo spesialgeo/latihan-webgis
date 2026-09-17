f05479fa287cd24da75a6e25f317d0280e5249ef
spesialgeo <spesialgeo@gmail.com>
2026-09-17 15:52:34 +0700

    up5

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { db } from "../../../../../lib/db";
-import bcryptjs from "bcryptjs"
+import bcrypt from "bcryptjs"
 
 
 export async function POST(request) {
